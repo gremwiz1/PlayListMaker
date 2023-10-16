19930a18cb5258783cfcefd01e1ce2574e0410cd
gremwiz1 <gremwiz@yandex.ru>
2023-10-16 12:07:08 +0300

    added search and save data from input

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,84 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val arrowBackButton = findViewById<ImageView>(R.id.backArrow)
+
+        arrowBackButton.setOnClickListener {
+            finish()
+        }
+
+        val linearLayout = findViewById<LinearLayout>(R.id.container)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            hideKeyboard()
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    // linearLayout.setBackgroundColor(getColor(R.color.prime_neutral))
+                } else {
+                    val input = s.toString()
+                }
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        //добавляем созданный simpleTextWatcher к EditText
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun hideKeyboard() {
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val inputText = inputEditText.text.toString()
+        outState.putString("inputText", inputText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val inputText = savedInstanceState.getString("inputText")
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        inputEditText.setText(inputText)
+    }
+
 }
