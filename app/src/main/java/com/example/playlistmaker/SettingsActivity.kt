0076200eb78aff031d2b6a32e9a4d84eb6424707
gremwiz1 <gremwiz@yandex.ru>
2023-10-16 08:39:08 +0300

    added functions on buttons

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,21 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.app.UiModeManager
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -14,6 +24,78 @@ class SettingsActivity : AppCompatActivity() {
 
         arrowBackButton.setOnClickListener {
             finish()
+        }
+        val writeSupportButton = findViewById<ImageView>(R.id.write_support)
+        writeSupportButton.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse("mailto:")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.eamil_rasrab)))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT,
+                getString(R.string.message_for_rasrab))
+            emailIntent.putExtra(Intent.EXTRA_TEXT,
+                getString((R.string.thanks_for_rasrab)))
+            if (emailIntent.resolveActivity(packageManager) != null) {
+                startActivity(emailIntent)
+            }
+        }
+
+
+        val switchDarkMode = findViewById<Switch>(R.id.switchDarkMode)
+
+        // Функция для определения текущего режима темы устройства
+        fun isNightModeEnabled(context: Context): Boolean {
+            val uiModeManager = context.getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+            val currentMode = uiModeManager.nightMode
+            return currentMode == 2
+        }
+
+        val sharedPreferences: SharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        val isFirstRun: Boolean = sharedPreferences.getBoolean("isFirstRun", true)
+
+        if (isFirstRun) {
+            // Экран запускается впервые
+            // Получаем текущий режим темы устройства
+            val isNightMode = isNightModeEnabled(this)
+            // Устанавливаем начальное состояние свитча в зависимости от текущего режима темы
+            switchDarkMode.isChecked = isNightMode
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putBoolean("isFirstRun", false)
+            editor.apply()
+        } else {
+            // Экран уже запускался ранее
+            // Получаем текущий режим темы
+            val currentNightMode = AppCompatDelegate.getDefaultNightMode()
+
+            if (currentNightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                // Текущая тема - темная (MODE_NIGHT_YES)
+               switchDarkMode.isChecked = true
+            } else {
+                // Текущая тема - светлая (MODE_NIGHT_NO)
+                switchDarkMode.isChecked = false
+            }
+        }
+
+
+        switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+        val agreementButton = findViewById<LinearLayout>(R.id.user_agreement)
+        agreementButton.setOnClickListener {
+            val url = Uri.parse(getString(R.string.agreement_link))
+            val agreementIntent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(agreementIntent)
+        }
+
+        val shareAppButton = findViewById<LinearLayout>(R.id.share_app)
+        shareAppButton.setOnClickListener() {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_link))
+            startActivity(shareIntent)
         }
     }
 }
