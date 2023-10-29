7b0cbf36e9a82ea266d22e9e685fbbdc5144ad28
gremwiz1 <gremwiz@yandex.ru>
2023-10-29 20:19:46 +0300

    fix styles with review demands and added check url on null

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -5,5 +5,5 @@ data class Track(
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
     val trackTimeMillis: String, // Продолжительность трека
-    val artworkUrl100: String, // Ссылка на изображение обложки
+    val artworkUrl100: String?, // Ссылка на изображение обложки
 )
